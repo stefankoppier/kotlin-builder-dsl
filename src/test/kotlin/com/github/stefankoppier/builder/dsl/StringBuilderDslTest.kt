f36d56b5bcb6a9487ff6b7dbf302027b76e989fe
stefankoppier <Ss#^PE50HDa4>
2022-12-05 12:24:32 +0100

    Extra tests for StringBuilderDsl

--- src/test/kotlin/com/github/stefankoppier/builder/dsl/StringBuilderDslTest.kt
+++ src/test/kotlin/com/github/stefankoppier/builder/dsl/StringBuilderDslTest.kt
@@ -3,25 +3,36 @@ package com.github.stefankoppier.builder.dsl
 import org.junit.jupiter.api.Test
 import org.mockito.Mockito
 import org.mockito.kotlin.whenever
-import java.math.BigDecimal
 import kotlin.test.assertEquals
+import kotlin.test.assertTrue
 
 class StringBuilderDslTest {
 
     @Test
-    fun `random`() {
+    fun random() {
         val faker = Mockito.spy(Faker::class.java)
         whenever(faker.nextString()).thenReturn("1")
         assertEquals("1", StringBuilderDsl(faker)())
     }
 
     @Test
-    fun `constant`() {
+    fun constant() {
         assertEquals("2", StringBuilderDsl().constant("2")())
     }
 
     @Test
-    fun `of`() {
+    fun format() {
+        val regex = "[aAzZ]+"
+        assertTrue { StringBuilderDsl().format(regex)().matches(Regex(regex))  }
+    }
+
+    @Test
+    fun `between 1 and 10`() {
+        assertTrue { (0..10).contains((String.of { between(1, 10)}).length) }
+    }
+
+    @Test
+    fun of() {
         assertEquals("3", String.of { constant("3") })
     }
 }
