7380551f266bfd48b03b582160523ed1903106d8
stefankoppier <Ss#^PE50HDa4>
2022-12-07 09:01:23 +0100

    Spotless

--- src/main/kotlin/com/github/stefankoppier/builder/dsl/primitives/DoubleBuilderDsl.kt
+++ src/main/kotlin/com/github/stefankoppier/builder/dsl/primitives/DoubleBuilderDsl.kt
@@ -3,20 +3,42 @@ package com.github.stefankoppier.builder.dsl.primitives
 import com.github.stefankoppier.builder.dsl.BuilderDsl
 import com.github.stefankoppier.builder.dsl.Faker
 
+/** DSL for building [Double] objects using the given [Faker]. */
 class DoubleBuilderDsl(private val faker: Faker = Faker()) : BuilderDsl<Double> {
 
     private var constant: Double? = null
 
+    /**
+     * Generates the object according to the provided instructions.
+     *
+     * @return A new [Double].
+     */
     override fun invoke(): Double {
         return constant ?: faker.double()
     }
 
+    /**
+     * Instruct the builder to generate a constant.
+     *
+     * @param value The value to generate.
+     *
+     * @return The DSL itself.
+     */
     fun constant(value: Double): DoubleBuilderDsl {
         constant = value
         return this
     }
 }
 
+/**
+ * Extension function for [Double.Companion] to simplify the DSL access.
+ *
+ * For example: `Double.of { constant(1) }`
+ *
+ * @param transform The instructions.
+ *
+ * @return A new [Double].
+ */
 fun Double.Companion.of(transform: DoubleBuilderDsl.() -> DoubleBuilderDsl = { DoubleBuilderDsl() }): Double {
     return transform(DoubleBuilderDsl())()
 }
