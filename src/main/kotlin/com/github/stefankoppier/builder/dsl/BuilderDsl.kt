08e37c40f0de79d7c3d2c6cb9646140365cee330
stefankoppier <Ss#^PE50HDa4>
2022-12-10 13:26:54 +0100

    Added documentation to BuilderDsl

--- src/main/kotlin/com/github/stefankoppier/builder/dsl/BuilderDsl.kt
+++ src/main/kotlin/com/github/stefankoppier/builder/dsl/BuilderDsl.kt
@@ -1,3 +1,8 @@
 package com.github.stefankoppier.builder.dsl
 
+/**
+ * Base interface for creating builders that can be used to generate values of type [T].
+ *
+ * @sample [com.github.stefankoppier.builder.dsl.primitives.BooleanBuilder]
+ */
 interface BuilderDsl<T> : () -> T
